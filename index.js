f16cddbe8611ddffa5d511451f9091b1446d2fa0
korniartem <artem115.dev@gmail.com>
2024-08-05 23:47:38 -0500

    DFEE

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors({
-  origin: 'https://ai.esmed.org', // Change this to your frontend origin
-}));
+// app.use(cors({
+//   origin: 'https://ai.esmed.org', // Change this to your frontend origin
+// }));
 app.use(express.json());
 
 const openai = new OpenAI({
