a6f0efc36596a7d4f3a15cb39a28f1c8299f3513
korniartem <artem115.dev@gmail.com>
2024-08-06 09:20:52 -0500

    added Header

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.use(cors());
 
 app.use(express.json());
